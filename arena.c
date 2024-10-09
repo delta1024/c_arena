6df291fe936458fcbe105a0e3e35a2da36d2959d
Jacob Stannix <jakestannix@gmail.com>
2024-10-09 12:48:22 -0600

    Added memory size tag to ptr header

--- arena.c
+++ arena.c
@@ -1,14 +1,15 @@
-#include <stdalign.h>
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "arena.h"
 #define ARENA_BLOCK_SIZE 2048
-typedef struct arena_alloc {
-	char *beg;
-	char *end;
-} arena_alloc;
+struct arena_block_tag {
+	ptrdiff_t block_size;
+};
+#define arena_get_tag_ptr(ptr) &((struct arena_block_tag*)(ptr))[-1]
 arena_alloc arena_init() {
 	arena_alloc a = {0};
 	a.beg = malloc(ARENA_BLOCK_SIZE);
@@ -17,11 +18,20 @@ arena_alloc arena_init() {
 }
 void *arena_raw_malloc(arena_alloc *a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count) {
 	ptrdiff_t padding = -(uintptr_t)a->beg & (align - 1);
+	// If alignment is bigger than padding size we store the tag in the padding
+	if (padding < sizeof(struct arena_block_tag)) {
+		// make room for the tag
+		a->beg += sizeof(struct arena_block_tag);
+		padding = -(uintptr_t)a->beg & (align - 1);
+
+	}
 	ptrdiff_t avalible = a->end - a->beg - padding;
 	if (avalible < 0 || count > avalible/size) {
 		abort();
 	}
 	void *p = a->beg + padding;
+	struct arena_block_tag *tag = arena_get_tag_ptr(p);
+	tag->block_size = count*size;
 	a->beg += padding + count*size;
 	return memset(p, 0, count*size);
 }
@@ -31,11 +41,12 @@ void arena_free(arena_alloc *a) {
 	void *malloc_ptr = a->beg - offset;
 	free(malloc_ptr);
 }
-#define arena_malloc(a, t, n) (t *)arena_raw_malloc(a, sizeof(t), alignof(t), n)
 int main() {
 	arena_alloc a = arena_init();
 	char *buff = arena_malloc(&a, char, strlen("hello") + 1 );
 	memcpy(buff, "hello", strlen("hello"));
 	printf("%s\n", buff);
+	struct arena_block_tag *tag = arena_get_tag_ptr(buff);
+	assert(tag->block_size == (strlen("hello") + 1) * sizeof(char));
 	arena_free(&a);
 } 
---
+++ arena.h
@@ -0,0 +1,13 @@
+#ifndef _ARENA_ALLOCATOR_H__
+#define _ARENA_ALLOCATOR_H__
+#include <stdalign.h>
+#include <stddef.h>
+typedef struct arena_alloc {
+	char *beg;
+	char *end;
+} arena_alloc;
+arena_alloc arena_init();
+void *arena_raw_malloc(arena_alloc *a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count);
+void arena_free(arena_alloc *a);
+#define arena_malloc(a, t, n) (t *)arena_raw_malloc(a, sizeof(t), alignof(t), n)
+#endif // _ARENA_ALLOCATOR_H__
